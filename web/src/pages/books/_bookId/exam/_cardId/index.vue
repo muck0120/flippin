7a80644d2f34b865ba27645d486d8f6d8b3a18c2
sawadamutsuki <mutsuki.s.0120@gmail.com>
2020-03-23 17:11:07 +0900

    [fix]テスト画面の表示崩れを修正
    テスト画面内のコンテンツ幅を横いっぱいになるように修正。

--- web/src/pages/books/_bookId/exam/_cardId/index.vue
+++ web/src/pages/books/_bookId/exam/_cardId/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :calss="$style.wrap">
     <HeaderBook
       :is-footer="false"
       :back-to="`/books/${book.book_id}/exam/cards`"
@@ -81,6 +81,11 @@ export default {
 </script>
 
 <style lang="scss" module>
+.wrap {
+  width: 100%;
+  height: 100%;
+}
+
 .question {
   margin-top: 30px;
 }
